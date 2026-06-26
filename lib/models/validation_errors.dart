7a5a875a0da15b80a816571b4916c3bc2d84edb3
Chea <Vithyeass@gmail.com>
2026-06-26 19:26:52 +0700

    create field for validation

---
+++ lib/models/validation_errors.dart
@@ -0,0 +1,22 @@
+enum ValidationsField {
+  username,
+  password,
+}
+
+class ValidationErrors {
+  final ValidationsField field;
+  final String message;
+
+  const ValidationErrors ({
+    required this.field,
+    required this.message,
+});
+
+  Map<String, String> toJson() {
+    return {
+      'field': field.name,
+      'message': message,
+    };
+  }
+
+}
